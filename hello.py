5534a01ca10db13a06c3309525b24fa8f4a044eb
Julia D <air-04@mail.ru>
2019-08-09 18:49:18 +0300

    gunicorn 5

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+#                   encoding="utf8")]
 def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    return [data]
