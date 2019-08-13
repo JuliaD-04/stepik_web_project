1835d8552e48b096016023454cc894a0d6c62879
Julia D <air-04@mail.ru>
2019-08-13 16:22:04 +0300

    last gunicorn

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-def app()
+                                                   
+
+def application(environ, start_response):
+                                                          
+		
+	status = '200 OK'
+
+	headers = [
+		('Content-Type', 'text/plain'),
+	]
+	body = "Hello world!"
+	start_response(status, response_headers)                                    
+
+	return [body]
