3039820c3a9a40098f631561210d7689062b27e3
Julia D <air-04@mail.ru>
2019-08-13 16:54:23 +0300

    last 4

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
                                                    
 
-def application(environ, start_response):
+# def application(environ, start_response):
                                                           
 		
-	status = '200 OK'
+# 	status = '200 OK'
 
-	headers = [
-		('Content-Type', 'text/plain'),
-	]
-	body = "Hello world!"
-	start_response(status, response_headers)                                    
+# 	headers = [
+# 		('Content-Type', 'text/plain'),
+# 	]
+# 	body = "Hello world!"
+# 	start_response(status, response_headers)                                    
 
-	return [body]
+# 	return [body]
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
