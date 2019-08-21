0ad6243b482591cc52205ebc8c77da4212e3e380
Julia D <air-04@mail.ru>
2019-08-21 18:05:15 +0300

    ooo4

--- hello.py
+++ hello.py
@@ -1,26 +1,12 @@
 
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    return (body)
+    def app(environ, start_response):
+		status = '200 OK'
 
-#     # return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-#     #               encoding="utf8")]
+		headers = [
+			('Content-Type', 'text/plain'),
+		]
+		body = "Hello world!"
+		start_response(status, response_headers)                                    
 
-
-
-
-
-    # def application(environ, start_response):
-                                                          
-		
-# 	status = '200 OK'
-
-# 	headers = [
-# 		('Content-Type', 'text/plain'),
-# 	]
-# 	body = "Hello world!"
-# 	start_response(status, response_headers)                                    
-
-# 	return [body]
+		return [body]
