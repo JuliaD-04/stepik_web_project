5c346b3b8ee107628133feae5ccc16c9caade519
Julia D <air-04@mail.ru>
2019-08-21 18:19:36 +0300

    ooo5

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /web/etc/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+
+
+# /etc/nginx/sites-enabled/default
