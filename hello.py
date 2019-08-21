2f579331a1d28f8ec3e9a6609ba07116b2f5d7a4
Julia D <air-04@mail.ru>
2019-08-21 17:34:03 +0300

    Ura

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    return body
+    return (body)
 
     # return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
     #               encoding="utf8")]
