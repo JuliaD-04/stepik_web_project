34b1ffbbdd04e313c21961660245c807d30dedba
Julia D <air-04@mail.ru>
2019-08-24 16:13:08 +0300

    2 exer

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /web/etc/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-
-
-# /etc/nginx/sites-enabled/default
