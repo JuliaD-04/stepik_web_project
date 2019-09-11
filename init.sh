fc77e6f449ea9839d1236840546b675e7ab6e675
Julia D <air-04@mail.ru>
2019-09-11 12:33:25 +0300

    2 razdel

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/hello.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/django.conf ask.wsgi:application
+
