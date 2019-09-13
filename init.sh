4a26a4a6eaf4ccc7a1627a652227f5648dc46eaa
Julia D <air-04@mail.ru>
2019-09-13 17:33:45 +0300

    3.1

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/django.conf /etc/gunicorn.d/views.py
+sudo ln -sf /home/box/web/etc/django.conf /etc/gunicorn.d/ask/qa/views.py.
 sudo /etc/init.d/gunicorn restart
