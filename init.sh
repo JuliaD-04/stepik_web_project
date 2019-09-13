44c82ea4800413eaabd8fd8b213b7b26cac92100
Julia D <air-04@mail.ru>
2019-09-13 17:27:30 +0300

    3

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django.conf /etc/gunicorn.d/views.py
 sudo /etc/init.d/gunicorn restart
