51f5265868e09d41499b0ed946c0dd5b37394ea5
Julia D <air-04@mail.ru>
2019-09-16 18:17:49 +0300

    5

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from qa.views import test
+# from . import views
+from django.urls import re_path
 
 urlpatterns = [
-    path('', test),
+    path('', test, ),
     re_path(r'^(?P<id>\d+)/$', test, name="test"),
    
     
